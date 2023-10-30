29c65e3b915167bec1c9cf36a2a4b6d0ab0bf959
guddu-123 <guddukr306@gmail.com>
2023-10-30 12:04:01 +0530

    Completed

---
+++ reverse.js
@@ -0,0 +1,5 @@
+let Str = "This is a sunny day";
+var words = Str.split("").reverse().join("").split(" ").reverse().join(" ");;
+console.log(words);
+
+
---
+++ sort.js
@@ -0,0 +1,2 @@
+const arr = [10,7,14,15,5,8,19];
+console.log(arr.sort(function(a,b){return b-a}));
